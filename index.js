0d22ad80e492961f7316dfd9125bbf2a9300c19f
ET45 <emre.tugutlu@gmail.com>
2022-05-08 16:37:00 +0300

    Information of characters sorted

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 //express Installed
 const express = require("express");
 
+const cors = require("cors");
+
 //axios Installed
 const axios = require("axios");
 
 //Express assigned to a constant
 const app = express();
 
+app.use(cors());
+
 //Port
 const port = 5000;
 
 //Endpoints
 app.get("/", async (req, res) => {
-  const url = "https://swapi.dev/api/films/3/?format=json";
+  const url = "https://swapi.dev/api/films/3/";
   const response = await axios.get(url);
   const characters = response.data.characters;
-  console.log(characters);
+  const pagination = characters.slice(0, 30);
+
+  //Info mapped and sorted
+  let charactersInfo = await Promise.all(
+    pagination.map((character) => axios.get(character))
+  );
+
+  charactersInfo = charactersInfo.map((character) => character.data);
+  console.log(charactersInfo);
+
+  /*   console.log(characters); */
+  res.status(200).send(charactersInfo);
 });
 
 //Listening Port
